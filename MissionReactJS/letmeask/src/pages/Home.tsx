a37f895924da2f07eb7b0606e3a925c39cf030d7
Bruno <brunocosta.1991@hotmail.com>
2022-02-10 22:34:49 -0300

    Contexto autenticação e recuperando estado login

--- MissionReactJS/letmeask/src/pages/Home.tsx
+++ MissionReactJS/letmeask/src/pages/Home.tsx
@@ -1,4 +1,6 @@
+import { useContext } from 'react'
 import { useHistory } from 'react-router-dom'
+import { AuthContext } from '../App'
 import googleIconImg from '../assets/images/google-icon.svg'
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -6,15 +8,20 @@ import { Button } from '../components/Button'
 import '../styles/auth.scss'
 
 
-export function Home(){
+
+export function Home() {
 
   const history = useHistory();
+  const { user, signInWithGoogle } = useContext(AuthContext)
 
-  function navigateToNewRoom(){
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
+    }
     history.push('/rooms/new');
   }
 
-  return(
+  return (
     <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
@@ -23,18 +30,18 @@ export function Home(){
       </aside>
       <main >
         <div className="main-content">
-          <img src={logoImg} alt='Letmeask'/>
-          <button onClick={navigateToNewRoom} className="create-room">
-            <img src={googleIconImg} alt= "Logo do Google"/>
+          <img src={logoImg} alt='Letmeask' />
+          <button onClick={handleCreateRoom} className="create-room">
+            <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
           <div className="separator">
             ou entre em uma sala
           </div>
           <form>
-            <input 
-            type="text"
-            placeholder="Digite o código da sala"
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
             />
             <Button type="submit">
               Entrar na sala
--- MissionReactJS/letmeask/src/pages/NewRoom.tsx
+++ MissionReactJS/letmeask/src/pages/NewRoom.tsx
@@ -1,13 +1,17 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
-import googleIconImg from '../assets/images/google-icon.svg'
+import { AuthContext } from '../App'
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import { Button } from '../components/Button'
 import '../styles/auth.scss'
 
 
-export function NewRoom(){
-  return(
+export function NewRoom() {
+
+  const { user } = useContext(AuthContext)
+
+  return (
     <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
@@ -16,12 +20,13 @@ export function NewRoom(){
       </aside>
       <main >
         <div className="main-content">
-          <img src={logoImg} alt='Letmeask'/>
+          <img src={logoImg} alt='Letmeask' />
+          <h1>{user?.name}</h1>
           <h2>Criar uma nova sala</h2>
           <form>
-            <input 
-            type="text"
-            placeholder="Nome da sala"
+            <input
+              type="text"
+              placeholder="Nome da sala"
             />
             <Button type="submit">
               Criar Sala
