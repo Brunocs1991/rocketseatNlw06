35779104c7c3425702d61e2aeae9b18507515016
Bruno <brunocosta.1991@hotmail.com>
2022-02-11 18:05:56 -0300

    recuperando questoes banco

--- MissionReactJS/letmeask/src/pages/NewRoom.tsx
+++ MissionReactJS/letmeask/src/pages/NewRoom.tsx
@@ -1,32 +1,35 @@
 import { FormEvent, useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
-import illustrationImg from '../assets/images/illustration.svg'
-import logoImg from '../assets/images/logo.svg'
-import { Button } from '../components/Button'
-import { useAuth } from '../hooks/useAuth'
-import { database } from '../services/firebase'
-import '../styles/auth.scss'
 
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/Button';
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
+
+import '../styles/auth.scss';
 
 export function NewRoom() {
-
-  const { user } = useAuth();
-  const history = useHistory();
+  const { user } = useAuth()
+  const history = useHistory()
   const [newRoom, setNewRoom] = useState('');
 
-  async function handlerCreateRoom(event: FormEvent) {
+  async function handleCreateRoom(event: FormEvent) {
     event.preventDefault();
+
     if (newRoom.trim() === '') {
       return;
     }
 
     const roomRef = database.ref('rooms');
+
     const firebaseRoom = await roomRef.push({
       title: newRoom,
       authorId: user?.id,
-    });
+    })
 
-    history.push(`/rooms/${firebaseRoom.key}`);
+    history.push(`/rooms/${firebaseRoom.key}`)
   }
 
   return (
@@ -34,21 +37,21 @@ export function NewRoom() {
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
-        <p>Tire as dúvidas da sua audiência em tempo-real.</p>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
-      <main >
+      <main>
         <div className="main-content">
-          <img src={logoImg} alt='Letmeask' />
+          <img src={logoImg} alt="Letmeask" />
           <h2>Criar uma nova sala</h2>
-          <form onSubmit={handlerCreateRoom}>
-            <input
+          <form onSubmit={handleCreateRoom}>
+            <input 
               type="text"
               placeholder="Nome da sala"
               onChange={event => setNewRoom(event.target.value)}
               value={newRoom}
             />
             <Button type="submit">
-              Criar Sala
+              Criar sala
             </Button>
           </form>
           <p>
