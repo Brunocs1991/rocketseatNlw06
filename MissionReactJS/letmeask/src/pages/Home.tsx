5f4785433a916c21d826452bac9ebfc554fae2b8
Bruno <brunocosta.1991@hotmail.com>
2022-02-12 12:08:56 -0300

    travando sala quando encerrar

--- MissionReactJS/letmeask/src/pages/Home.tsx
+++ MissionReactJS/letmeask/src/pages/Home.tsx
@@ -39,6 +39,10 @@ export function Home() {
       return;
     }
 
+    if(roomRef.val().endedAt){
+      alert('Room already closed');
+      return;
+    }
     history.push(`/rooms/${roomCode}`);
   }
 
