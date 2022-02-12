10bd3ec2930f5ce73d2ba2e80bbcceddbb25443b
Bruno <brunocosta.1991@hotmail.com>
2022-02-12 13:01:01 -0300

    Finalizando projeto

--- MissionReactJS/letmeask/src/components/Question/index.tsx
+++ MissionReactJS/letmeask/src/components/Question/index.tsx
@@ -1,26 +1,32 @@
 
 import { ReactNode } from 'react';
+import cx from 'classnames'
 import './styles.scss'
+
 type QuestionProps = {
   content: string;
-  author:{
+  author: {
     name: string;
     avatar: string;
   };
-  children?: ReactNode
+  children?: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
 }
 
 export function Question({
   content,
   author,
+  isAnswered = false,
+  isHighlighted = false,
   children
-}: QuestionProps){
-  return(
-    <div className="question">
+}: QuestionProps) {
+  return (
+    <div className={cx('question', { answered: isAnswered }, { highlighted: isHighlighted && !isAnswered },)}>
       <p>{content}</p>
       <footer>
         <div className="user-info">
-          <img src={author.avatar} alt= {author.name}/>
+          <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
         <div>
